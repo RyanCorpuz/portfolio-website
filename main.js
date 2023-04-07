5e81fba88d3e4d5314ac446b5a0037f067d32e72
RyanCorpuz <ryancorpuz@Ryans-MacBook-Pro.local>
2023-04-07 05:00:41 -0600

    Completed About section, contact buttons and construction screen

--- main.js
+++ main.js
@@ -18,3 +18,49 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+const toggleBtn = document.querySelector(".toggle_btn")
+const toggleBtnIcon = document.querySelector(".toggle_btn i")
+const dropDownMenu = document.querySelector(".dropdown_menu")
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open")
+  const isOpen = dropDownMenu.classList.contains("open")
+
+  toggleBtnIcon.classList = isOpen 
+    ? "fa-solid fa-xmark" : 
+  "fa-solid fa-bars";
+}
+
+window.addEventListener('click', function(e){
+    if (!document.getElementById('dropdown_menu').contains(e.target) && (!document.getElementById('navbar').contains(e.target)) && dropDownMenu.classList.contains("open")){
+    dropDownMenu.classList.toggle("open")
+    const isOpen = dropDownMenu.classList.contains("open")
+  
+    toggleBtnIcon.classList = isOpen 
+      ? "fa-solid fa-xmark" : 
+    "fa-solid fa-bars"; //the same code you've used to hide the menu
+  } 
+})
+
+const links = document.querySelectorAll(".droplink")
+
+links.forEach( l => l.addEventListener("click", function(e) {
+    dropDownMenu.classList.toggle("open")
+    const isOpen = dropDownMenu.classList.contains("open")
+  
+    toggleBtnIcon.classList = isOpen 
+      ? "fa-solid fa-xmark" : 
+    "fa-solid fa-bars"; //the same code you've used to hide the menu
+  
+}));
+
+window.addEventListener("scroll", function (e){
+    const isOpen = dropDownMenu.classList.contains("open")
+    if(isOpen) {
+        dropDownMenu.classList.toggle("open")
+    }
+    toggleBtnIcon.classList = isOpen 
+        ? "fa-solid fa-xmark" : 
+      "fa-solid fa-bars";
+})
